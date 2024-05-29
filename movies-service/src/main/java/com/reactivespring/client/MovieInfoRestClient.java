386c70324234b221d4d939a7afc5e614536259ae
Kyra <kyra.dev.07@gmail.com>
2024-05-28 21:08:50 -0500

    Se implementan servicios rest para hacer las peticiones a los respectivos microservicios, movie info y reviews

--- movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
@@ -1,2 +1,30 @@
-package com.reactivespring.client;public class MovieInfoRestClient {
+package com.reactivespring.client;
+
+import com.reactivespring.domain.MovieInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MovieInfoRestClient {
+
+    private final WebClient webClient;
+
+    @Value("${restClient.movieInfoUrl}")
+    private String MOVIE_INFO_URL;
+
+    public MovieInfoRestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<MovieInfo> retrieveMovieInfo(String movieId) {
+        String url = MOVIE_INFO_URL + "/{id}";
+        return this.webClient
+                .get()
+                .uri(url, movieId)
+                .retrieve()
+                .bodyToMono(MovieInfo.class)
+                .log();
+    }
 }
--- movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
@@ -1,2 +1,36 @@
-package com.reactivespring.client;public class ReviewRestClient {
+package com.reactivespring.client;
+
+import com.reactivespring.domain.Review;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+
+@Component
+public class ReviewRestClient {
+
+    private final WebClient webClient;
+
+    @Value("${restClient.reviewUrl}")
+    private String REVIEW_URL;
+
+    public ReviewRestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Flux<Review> retrieveReviews(String movieInfoId) {
+        String uri = UriComponentsBuilder
+                .fromHttpUrl(REVIEW_URL)
+                .queryParam("id", movieInfoId)
+                .buildAndExpand()
+                .toUriString();
+
+        return this.webClient
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToFlux(Review.class);
+
+    }
 }
