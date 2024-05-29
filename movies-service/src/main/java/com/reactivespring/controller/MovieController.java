f23cc9d2c044e6365930b1413a7d4541701fe1ca
Kyra <kyra.dev.07@gmail.com>
2024-05-28 21:52:07 -0500

    Se implementa método para consulta tanto la información de las películas como sus reviews

--- movies-service/src/main/java/com/reactivespring/controller/MovieController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MovieController.java
@@ -1,2 +1,31 @@
-package com.reactivespring.controller;public class MovieController {
+package com.reactivespring.controller;
+
+import com.reactivespring.client.MovieInfoRestClient;
+import com.reactivespring.client.ReviewRestClient;
+import com.reactivespring.domain.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("v1/movie")
+public class MovieController {
+
+    private final MovieInfoRestClient movieInfoRestClient;
+    private final ReviewRestClient reviewRestClient;
+
+    public MovieController(MovieInfoRestClient movieInfoRestClient, ReviewRestClient reviewRestClient) {
+        this.movieInfoRestClient = movieInfoRestClient;
+        this.reviewRestClient = reviewRestClient;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Movie> retrieveMovieById(@PathVariable String id) {
+        return this.movieInfoRestClient.retrieveMovieInfo(id)
+                .flatMap(movieInfo -> this.reviewRestClient.retrieveReviews(movieInfo.getMovieInfoId())
+                        .collectList()
+                        .map(reviewsList -> new Movie(movieInfo, reviewsList)));
+    }
 }
