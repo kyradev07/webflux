49db198608b1830a9206a996cb7b62032058be7e
Kyra <kyra.dev.07@gmail.com>
2024-06-12 00:26:48 -0500

    Se crean endpoint para recibir actualizaciones constantes cuando se crea una pelicula para consultarlas desde el servicio de películas

--- movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
@@ -1,5 +1,6 @@
 package com.reactivespring.client;
 
+import com.reactivespring.domain.Movie;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
 import com.reactivespring.exception.MoviesInfoServerException;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -32,7 +34,7 @@ public class MovieInfoRestClient {
                 .uri(url, movieId)
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
-                    if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
                         return Mono.error(new MoviesInfoClientException(
                                 "There is not MovieInfo for id " + movieId,
                                 clientResponse.statusCode().value()));
@@ -43,12 +45,29 @@ public class MovieInfoRestClient {
                                     responseMessage, clientResponse.statusCode().value()
                             )));
                 })
-                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
-                    return clientResponse.bodyToMono(String.class)
-                            .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException(
-                                    "Server Exception in MovieInfoService -> " + responseMessage)));
-                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse ->
+                        clientResponse.bodyToMono(String.class)
+                                .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException("Server Exception in MovieInfoService -> " + responseMessage))))
                 .bodyToMono(MovieInfo.class)
+                .retryWhen(RetryUtil.retrySpec())
+                .log();
+    }
+
+    public Flux<MovieInfo> retrieveMovieInfoStream() {
+        return this.webClient
+                .get()
+                .uri(this.MOVIE_INFO_URL + "/stream")
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                   return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                    responseMessage, clientResponse.statusCode().value()
+                            )));
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse ->
+                        clientResponse.bodyToMono(String.class)
+                                .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException("Server Exception in MovieInfoService -> " + responseMessage))))
+                .bodyToFlux(MovieInfo.class)
                 .retryWhen(RetryUtil.retrySpec())
                 .log();
     }
--- movies-service/src/main/java/com/reactivespring/controller/MovieController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MovieController.java
@@ -3,10 +3,13 @@ package com.reactivespring.controller;
 import com.reactivespring.client.MovieInfoRestClient;
 import com.reactivespring.client.ReviewRestClient;
 import com.reactivespring.domain.Movie;
+import com.reactivespring.domain.MovieInfo;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -27,5 +30,10 @@ public class MovieController {
                 .flatMap(movieInfo -> this.reviewRestClient.retrieveReviews(movieInfo.getMovieInfoId())
                         .collectList()
                         .map(reviewsList -> new Movie(movieInfo, reviewsList)));
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<MovieInfo> retrieveMovieInfoStream() {
+        return this.movieInfoRestClient.retrieveMovieInfoStream();
     }
 }
